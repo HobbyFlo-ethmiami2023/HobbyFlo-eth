97a5ba9e012e5f17fab3bdc0d7fb20f1f2df83bc
tad dy <a@a.com>
2023-10-28 15:01:15 -0400

    tweak the landing page header

--- cashflo-web/components/indexComponents/hero.tsx
+++ cashflo-web/components/indexComponents/hero.tsx
@@ -6,7 +6,7 @@ export const Hero = () => {
       <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
         <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
           <img
-            src="/flo_rida_swole.png"
+            src="/cashflo_logo.jpeg"
             alt=""
             className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"
           />
