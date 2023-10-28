e2eca45aa0a41325d505e0e545172a0ab275c5ce
Julio M Cruz <julio.cruz@eb-ms.net>
2023-10-28 17:34:33 -0400

    Update social login

--- cashflo-web/pages/_app.tsx
+++ cashflo-web/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { getDefaultWallets, RainbowKitProvider, connectorsForWallets } from '@rainbow-me/rainbowkit';
 import type { AppProps } from 'next/app';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import {
@@ -13,6 +13,30 @@ import {
   celoAlfajores,
 } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
+
+import {
+  argentWallet,
+  coinbaseWallet,
+  imTokenWallet,
+  injectedWallet,
+  ledgerWallet,
+  metaMaskWallet,
+  omniWallet,
+  rainbowWallet,
+  trustWallet,
+  walletConnectWallet,
+} from '@rainbow-me/rainbowkit/wallets';
+
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+
+import { ParticleNetwork } from '@particle-network/auth';
+import { particleWallet } from '@particle-network/rainbowkit-ext';
+
+new ParticleNetwork({
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID as string,
+  clientKey: process.env.NEXT_PUBLIC_CLIENT_KEY as string,
+  appId: process.env.NEXT_PUBLIC_APP_ID as string,
+});
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [
@@ -27,11 +51,46 @@ const { chains, publicClient, webSocketPublicClient } = configureChains(
   [publicProvider()]
 );
 
-const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
-  chains,
-});
+const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "";
+
+const particleWallets = [
+  particleWallet({ chains, authType: 'google' }),
+  particleWallet({ chains, authType: 'facebook' }),
+  particleWallet({ chains, authType: 'apple' }),
+  particleWallet({ chains }),
+];
+
+const popularWallets = {
+  groupName: 'Popular',
+  wallets: [
+      ...particleWallets,
+      injectedWallet({ chains }),
+      rainbowWallet({ chains, projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string }),
+      coinbaseWallet({ appName: 'RainbowKit demo', chains }),
+      metaMaskWallet({ chains, projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string }),
+      walletConnectWallet({ chains, projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string }),
+  ],
+};
+
+// const { connectors } = getDefaultWallets({
+//   appName: 'RainbowKit App',
+//   projectId: 'YOUR_PROJECT_ID',
+//   chains,
+// });
+
+const connectors = connectorsForWallets([
+  popularWallets,
+  {
+      groupName: 'Other',
+      wallets: [
+          argentWallet({ chains, projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string }),
+          trustWallet({ chains, projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string }),
+          omniWallet({ chains, projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string }),
+          imTokenWallet({ chains, projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string }),
+          ledgerWallet({ chains, projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string }),
+      ],
+  },
+]);
 
 const wagmiConfig = createConfig({
   autoConnect: true,
