cc3460e1ce0288818a09fec455ccc1709f4e8097
patrickbdevaney <79435941+patrickbdevaney@users.noreply.github.com>
2023-10-29 03:57:35 -0400

    figured out bytes32 input leading 0x for SHA hash and made arbitration boolean

--- hobbyflo-contracts/Deal.sol
+++ hobbyflo-contracts/Deal.sol
@@ -4,94 +4,94 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract Deal {
 
-    // escrow 
+// escrow
     uint256 public escrow;
 
-    // Buyer and seller state variables
+// Buyer and seller state variables
     address public buyer;
     address public seller;
 
-    // Milestones
-    enum DealMilestone {
-        AgreementRetainer,
-        InspectionPeriodOpens,
-        InspectionPeriodCloses,
-        DealCloses
-    }
+// inspection start date timestamp
+    uint256 public inspectionStartDate;
 
-    // storing results of arbitration
-    enum ArbitrationStatus {
-        Pending,
-        BuyerWins,
-        SellerWins
-    }
-    ArbitrationStatus public arbitrationStatus;
+// inspection end date timestamp
+    uint256 public inspectionEndDate;
 
-    // jury DAO 
+// closing date timestamp
+    uint256 public closingDate;
+
+// arbitration flag
+    bool public arbitrationFlag;
+
+// jury DAO
     address public daoAddress;
 
-    // milestones stored on chain
-    DealMilestone[] public milestones;
+//input is leading 0x followed by a hexadecimal conversion of your hash (SHA256, SHA3, MD5, CRC)
+    bytes32 hash2;
 
+// milestones calculated using block.timestamp
     constructor(
         address _buyer,
         address _seller,
+        address _daoAddress,
         uint256 _escrow,
-        DealMilestone[] memory _milestones
+        bytes32 _hash2
+       
     ) {
         buyer = _buyer;
         seller = _seller;
         escrow = _escrow;
-        milestones = _milestones;
+        daoAddress= _daoAddress;
+       
+        hash2= _hash2;
 
-        // defaults arbitration to pending
-        arbitrationStatus = ArbitrationStatus.Pending;
+// Set the milestones based on the current timestamp
+        inspectionStartDate = block.timestamp;
+        inspectionEndDate = inspectionStartDate + 1209600;
+        closingDate = inspectionEndDate + 604800;
+
+// default arbitration to pending
+        arbitrationFlag = false;
     }
 
-    // escrow deposit
+// escrow deposit
     function deposit() external payable {
         require(msg.sender == buyer, "Only the buyer deposits escrow");
         require(msg.value == escrow, "The deposit must match the escrow amount");
     }
 
-    function releaseEscrowToSellerAtClosingDate() external {
-  require(msg.sender == buyer, "Only the buyer can release escrow to seller at closing date");
-  require(arbitrationStatus == ArbitrationStatus.Pending, "Arbitration is in progress");
-     // Release the escrow funds to the seller
-  payable(seller).transfer(escrow);
-}
+// Close the deal and pay the seller the escrow amount if the closing date has been reached
+    function closeDeal() external {
+        require(msg.sender == buyer, "Only the buyer can close the deal");
+        require(block.timestamp >= closingDate, "The closing date has not been reached");
 
+// Release the escrow funds to the seller
+        payable(seller).transfer(escrow);
+    }
 
-
-    // Start arbitration
+// Start arbitration
     function startArbitration() external {
         require(msg.sender == buyer || msg.sender == seller, "Only the buyer or seller can start arbitration");
-        require(arbitrationStatus == ArbitrationStatus.Pending, "Arbitration is already in progress");
+        require(arbitrationFlag == false, "Arbitration is already in progress");
 
-        //arbitration handled by DAO internally
-
-        // set the arbitration status to pending
-        arbitrationStatus = ArbitrationStatus.Pending;
+// set the arbitration flag to true
+        arbitrationFlag = true;
     }
-    
 
-    // Handle verdict of arbitration. The DAO calls the function with result of arbitration and winner address as parameters
-   function handleArbitrationResults(ArbitrationStatus result, address winner) external {
-  require(msg.sender == daoAddress, "Only the DAO can handle arbitration results");
+function handleArbitrationResults(address winner) external {
+        require(msg.sender == daoAddress, "Only the DAO can handle arbitration results");
+        require(arbitrationFlag == true, "Arbitration is not in progress");
 
-  // Calculate the DAO fee
-  uint256 daoFee = escrow * 5 / 100;
+// Calculate the DAO fee
+        uint256 daoFee = escrow * 5 / 100;
 
-  // Subtract the DAO fee from the escrow amount
-  escrow -= daoFee;
+// Subtract the DAO fee from the escrow amount
+        escrow -= daoFee;
 
-  // Update the arbitration status
-  arbitrationStatus = result;
+// Release the escrow funds to the winner
+        payable(winner).transfer(escrow);
 
-  // Release the escrow funds to the winner
-  if (winner != address(0)) {
-    payable(winner).transfer(escrow);
-    
-  }
-}
+// set the arbitration flag to false
+        arbitrationFlag = false;
+    }
 }
